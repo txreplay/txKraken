578e7facdbe614d01224b385b8a1333ba48f4fd3
Valentin Commenge <txreplay@gmail.com>
2017-10-16 20:31:19 +0200

    Base template

--- src/AppBundle/Controller/FrontController.php
+++ src/AppBundle/Controller/FrontController.php
@@ -6,7 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
+class FrontController extends Controller
 {
     /**
      * @Route("/", name="homepage")
@@ -14,8 +14,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        return $this->render('front/index.html.twig', [
+
         ]);
     }
 }
